1f976a9444070a2602572bf4ccb2b06f67a76be8
Janne Peippo <peippo@gmail.com>
2023-08-14 21:06:24 +0300

    Initialize project

---
+++ src/yahtzure/core.cljs
@@ -0,0 +1,19 @@
+(ns yahtzure.core
+    (:require
+      [reagent.core :as r]
+      [reagent.dom :as d]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h1 "Yahtzure"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
